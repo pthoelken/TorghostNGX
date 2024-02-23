3d2942f5a662ab0b1d5460a7adc545020ac52457
Patrick Thoelken <patrick.thoelken@outlook.com>
2024-02-23 09:42:34 +0100

    update buildfile

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ TorGhost is an anonymization script. TorGhost redirects all internet traffic thr
 Project is currecntly in development
 
 
+
+
+Requirement already satisfied: cython in /usr/local/lib/python3.11/dist-packages (from -r requirements.txt (line 3)) (3.0.8)
+WARNING: Running pip as the 'root' user can result in broken permissions and conflicting behaviour with the system package manager. It is recommended to use a virtual environment instead: https://pip.pypa.io/warnings/venv
+Compiling /root/TorghostNGX/torghost.py
+/usr/local/lib/python3.11/dist-packages/Cython/Compiler/Main.py:381: FutureWarning: Cython directive 'language_level' not set, using '3str' for now (Py3). This has changed from earlier releases! File: /root/TorghostNGX/torghost.py
+  tree = Parsing.p_module(s, pxd, full_module_name)
--- build.sh
+++ build.sh
@@ -1,12 +1,48 @@
-#!/bin/bash -e
+#!/bin/bash
 
-sudo apt-get install tor python3-pip cython3 -y 
-sudo pip3 install -r requirements.txt 
-mkdir build && cd build
+strBuildDirectory=build
+objPythonVersion=$(python3 -V | cut -d' ' -f2 | cut -d'.' -f1,2)
 
-py3_version=$(python3 -V | cut -d' ' -f2 | cut -d'.' -f1,2)
+if [ -d $strBuildDirectory ]; then
+    rm -rf $strBuildDirectory
+    mkdir $strBuildDirectory
+    cd $strBuildDirectory
+fi
 
-cython3 ../torghost.py --embed -o torghost.c --verbose
-gcc -Os -I /usr/include/python${py3_version} -o torghost torghost.c -lpython${py3_version} -lpthread -lm -lutil -ldl
-sudo cp -r torghost /usr/bin/
-chmod +x /usr/bin/torghost
+if [ "$EUID" -ne 0 ]; then 
+    echo "[ERROR] Operation $1 was not successfull!"
+    exit 1
+fi
+
+function ProgressCheck() {
+    if [ $? -eq 0 ]; then
+        echo -e "[SUCCESS] Operation | $1 | was successfull!"
+    else
+        echo -e "[ERROR] Operation | $1 | was not successfull!"
+        exit 1
+    fi
+}
+
+function main() {
+    apt update
+    ProgressCheck "Repository Update"
+
+    apt install tor python3-pip cython3 -y
+    ProgressCheck "Install System requirements"
+
+    pip3 install -r requirements.txt 
+    ProgressCheck "Install Python requirements"
+
+    cython3 -3 ../torghost.py --embed -o torghost.c --verbose
+    ProgressCheck "Generate C Code"
+
+    gcc -Os -I /usr/include/python${objPythonVersion} -o torghost torghost.c -lpython${objPythonVersion} -lpthread -lm -lutil -ldl
+    ProgressCheck "Compiled to static binary"
+
+    sudo cp -r torghost /usr/bin/
+    chmod +x /usr/bin/torghost
+    ProgressCheck "Install to /usr/bin/ and make it executable"
+}
+
+main
+exit 0
